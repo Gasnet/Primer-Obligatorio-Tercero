cc7382eac993acc2b883a53d6215c7d5716d8af6
Gaston <gasnet@adinet.com.uy>
2010-06-15 23:30:20 -0300

    agregue toDTUsuario():void

--- src/models/UOperador.java
+++ src/models/UOperador.java
@@ -5,6 +5,9 @@
 
 package models;
 
+import dataTypes.DTUsuario;
+import dataTypes.TipoUsuario;
+
 /**
  *
  * @author gaston
@@ -13,6 +16,10 @@ public class UOperador extends Usuario{
 
     public UOperador(String nombre, String password, int cedula) {
         super(nombre, password, cedula);
+    }
+
+    public DTUsuario toDTUsuario(){
+        return new DTUsuario(getNombre(), getPassword(), getCedula(), TipoUsuario.UOperador);
     }
 
 }
